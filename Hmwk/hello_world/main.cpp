fbfc1a495dc6198fe04c9098afcb1ba9725b4484
zach5456 <zstewart3@student.rcc.edu>
2014-06-25 19:41:09 -0700

    Signed-off-by: zach5456 <zstewart3@student.rcc.edu>

---
+++ Hmwk/hello_world/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 25, 2014, 7:21 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"hello world"<<endl;
+    return 0;
+}
+
