722aa16f349aa54a702aac3a5ac00ab4ab7df54d
zach5456 <zstewart3@student.rcc.edu>
2014-07-02 16:28:31 -0700

    Assignment2

--- Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob4/main.cpp
+++ Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob4/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 /*
@@ -15,20 +16,20 @@ using namespace std;
  */
 int main() {
   
-  int monthA, monthB, monthC;
+  string monthA, monthB, monthC;
   float average, rainA, rainB, rainC;
   
   cout << "Enter the first month you want to calculate the amount of rain ";
   cout << "for. \n";
-  cin >> monthA;
+  getline(cin, monthA);
   
   cout << "Enter the second month you want to calculate the amount of rain ";
   cout <<"for. \n";
-  cin >> monthB;
+  getline(cin, monthB);
           
   cout << "Enter the third month you want to calculate the amount of rain ";
   cout <<"for. \n";
-  cin >> monthC;
+  getline(cin, monthC);
           
   cout << "Enter the amount of rain for the first month. \n";
   cin >> rainA;
@@ -43,8 +44,7 @@ int main() {
   
   cout << setprecision(2) << fixed;
   cout << "The average rain for, " << monthA << ", " << monthB << ", and ",
-  cout << monthC << "is " << average << "." << endl;
+  cout << monthC << " is " << average << "." << endl;
 
   return 0;
 }
-
--- Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob5/main.cpp
+++ Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob5/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 int main() {
 
   string movieName;
-  float ticketAdult = 6.00, ticketChild = 3.00, gross, net, amountPaid;
+  float ticketAdult, ticketChild, gross, net, amountPaid;
   
   cout << "Enter the name of the movie you'd like to calculate income." << endl;
   getline(cin, movieName);
@@ -29,15 +29,15 @@ int main() {
   cout << "Enter the amount of child tickets sold." << endl;
   cin >>ticketChild;
   
-  gross = ticketAdult + ticketChild;
+  gross = (ticketAdult * 6.00) + (ticketChild * 3.00);
   cout << "The gross amount made was $" << gross << "." << endl;
   
   amountPaid = gross * 0.20;
-  net = gross - amountPaid;
+  net = gross * 0.20;
   cout << setprecision(6) << showpoint << net << endl;
   cout << "The net amount made is $" << net << "." << endl;
   
-  amountPaid = gross * 0.20;
+  amountPaid = gross - amountPaid;
   cout << setprecision(6) << showpoint << amountPaid << endl;
   cout << "The amount paid is $" << amountPaid << "." << endl;
   
