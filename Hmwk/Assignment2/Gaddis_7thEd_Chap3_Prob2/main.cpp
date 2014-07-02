3dedcd2b5742a33237be0548b8e57b7704a670ca
zach5456 <zstewart3@student.rcc.edu>
2014-07-02 11:32:59 -0700

    Assignment2

--- Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob2/main.cpp
+++ Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob2/main.cpp
@@ -1,19 +1,42 @@
 /* 
- * File:   main.cpp
- * Author: zach5456
+  * File:   main.cpp
+ * Author: Stewart, Zach
+ * Class: 46024
  *
  * Created on July 2, 2014, 10:47 AM
  */
 
-#include <cstdlib>
-
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+int main() {
 
+  int a, b, c;
+  float classA, classB, classC, sales;
+  
+  cout << "This program calculates the total amount of ticket sales. \n";
+  cout << "Enter the amount of Class A tickets sold. \n";
+  cin >> a;
+         
+  cout << "Enter the amount of Class B tickets sold. \n";
+  cin >> b;
+  
+  cout << "Enter the amount of Class C tickets sold. \n";
+  cin >> c;
+  
+  classA = a * 15.00;
+  classB = b * 12.00;
+  classC = c * 9.00;
+  
+  sales = classA + classB + classC;
+   
+  cout << setprecision(2) << fixed;
+  cout << "The total amount is $ " << sales << "." << endl;
+  
   return 0;
 }
 
---
+++ Hmwk/Assignment2/Gaddis_7thEd_Chap3_Prob3/main.cpp
@@ -0,0 +1,44 @@
+/* 
+   * File:   main.cpp
+ * Author: Stewart, Zach
+ * Class: 46024
+ *
+ * Created on July 2, 2014, 11:04 AM
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+  float testA, testB, testC, testD, testE;
+  float average;
+  
+  cout << "This program will calculate the average test score. \n";
+  cout << "Enter the score of the first test. \n";
+  cin >> testA;
+    
+  cout << "Enter the score of the second test. \n";
+  cin >> testB;
+          
+  cout << "Enter the score of the third test. \n";
+  cin >> testC;
+          
+  cout << "Enter the score of the fourth test. \n";
+  cin >> testD;
+  
+  cout << "Enter the score of the fifth test. \n";
+  cin >> testE;
+
+  average = (testA + testB + testC + testD + testE) / 5.0;
+   
+  cout << setprecision(1) << fixed;
+  cout << "The average of the tests is " << average << "." << endl;
+          
+  return 0;
+}
+
