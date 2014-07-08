a1078711458c88c106578cac49af2b2178c3a53b
zach5456 <zstewart3@student.rcc.edu>
2014-07-07 20:42:07 -0700

    Assignment3

---
+++ Hmwk/Assignment3/Gaddis_7thEd_Chap3_Prob2/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Stewart, Zach
+ * Class: 46024
+ *
+ * Created on July 7, 2014, 8:11 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    
+    
+    return 0;
+}
+
---
+++ Hmwk/Assignment3/Gaddis_7thEd_Chap3_Prob3/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Stewart, Zach
+ * Class: 46024
+ *
+ * Created on July 7, 2014, 8:18 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    int day, month, year;
+    
+    cout << "Enter a day." << endl;
+    cin >> day;
+    cout << "Enter a month." << endl;
+    cin >> month;
+    cout << "Enter a year." << endl;
+    cin >> year;
+    
+    if (year = day * month);
+        cout << "This is a magic date!" << endl;
+    else
+        cout << "The date is " << month << "/" << day << "/" << year << endl;      
+       return 0;
+}
+
