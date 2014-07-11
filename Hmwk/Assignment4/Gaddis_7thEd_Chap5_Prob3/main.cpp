87045b812c8f40d6f9cec964107e8ac024c7095a
zach5456 <zstewart3@student.rcc.edu>
2014-07-10 23:41:49 -0700

    Assignment4

---
+++ Hmwk/Assignment4/Gaddis_7thEd_Chap5_Prob3/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Stewart, Zach
+ * Class: 46024
+ *
+ * Created on July 10, 2014, 11:11 PM
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+  const int START_YEAR = 1;
+  const int END_YEAR = 25;
+  float rise = 1.5, total, year;
+  
+  cout << fixed << showpoint << setprecision(1);
+  
+  cout << "Year\tRise" << endl;
+  
+  for (int year = START_YEAR; year <= END_YEAR; year++)
+  {
+    total = year * rise;
+    cout << year << "\t" << total << endl;
+  }
+  
+  return 0;
+}
+
